575fe33a695ea4b69ed796700c8eb6252067fcfa
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2015-08-27 21:32:13 +0900

    modified:   test.py

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ for i in ["aa", "ii", "uu"]:
   if i == "aa":
     print i , "1234567890"[:1:]
   elif i == "ii":
-    print i,"1234567890"[:-3]
+    print i,"1234567890"[:-3][2:]
   else:
     print i,    "1234567890"[-3:]

d94b3e197ca84dd2b74d7f5da459ee8fcd089820
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2015-09-13 22:28:07 +0900

    modified:   test.py

--- test.py
+++ test.py
@@ -3,16 +3,21 @@ b = [3, 2, 1]
 print a, b
 print 'a' 'b''c'
 
+print {x: x**2 for x in (2, 4, 6, 8 ,10)}
+print [x**2 for x in (2, 4, 6)]
+print [x**3 for x in range(10) if x == x**2]
+
 for n, i in enumerate(["aa", "ii", "uu"]):
-  if i == "aa":
+
+  if i == "uu":
     print n, i , "1234567890"[:-3]
-  elif i == "ii":
+  elif i == "aa":
     print n,i, "1234567890"[:-3][2:]
   else:
     print n,      i, "1234567890"[-3][2:][:1:2][:]
     print "ee""oo","ee\"\"oo"
 
-print {x: x**2 for x in (2, 4, 6, 8 ,10)}
-print [x**2 for x in (2, 4, 6)]
-print [x**3 for x in range(10) if x == x**2]
+
+
+    print "aiueo"
 
